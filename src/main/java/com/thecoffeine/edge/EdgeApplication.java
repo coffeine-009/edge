fd95ad920f573d0d1c41d178c083d976347322d5
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2017-08-23 09:30:40 +0300

    SSO setup.

--- src/main/java/com/thecoffeine/edge/EdgeApplication.java
+++ src/main/java/com/thecoffeine/edge/EdgeApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * Edge server.
@@ -13,14 +15,27 @@ import org.springframework.stereotype.Controller;
  * @version 1.0
  */
 @SpringBootApplication
-@Controller
 @EnableOAuth2Sso
 @EnableZuulProxy
-public class EdgeApplication {
+public class EdgeApplication extends WebSecurityConfigurerAdapter {
 
     public static void main( String [] args ) {
         new SpringApplicationBuilder( EdgeApplication.class )
             .web( true )
             .run( args );
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            .logout().and()
+            .authorizeRequests()
+            .antMatchers( "/", "/login").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .csrf()
+            .csrfTokenRepository( CookieCsrfTokenRepository.withHttpOnlyFalse());
+        // @formatter:on
     }
 }
--- src/test/java/com/thecoffeine/edge/EdgeApplicationTest.java
+++ src/test/java/com/thecoffeine/edge/EdgeApplicationTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -32,7 +33,7 @@ public class EdgeApplicationTest {
         this.mockMvc.perform(
             get( "/info" )
                 .contentType( MediaType.APPLICATION_JSON_UTF8 )
-        )
+        ).andDo( print() )
             .andExpect( status().isFound() );
     }
 }
